d9815f40c0ac283d795faeaa039aeb9867241281
emma@beinproximity.com <emma@beinproximity.com>
2022-04-28 08:14:30 -0600

    added more to the mvc file structure

--- develop/models/Thought.js
+++ develop/models/Thought.js
@@ -1,30 +1,22 @@
 const mongoose = require('mongoose');
 
 const reactionSchema = new mongoose.Schema({
-    // TODO: Use Mongoose's ObjectId data type
-    // Default value is set to a new ObjectId
+    // TODO: Use Mongoose's ObjectId data type, Default value is set to a new ObjectId
     reactionID: { type: String, required: true },
-    // TODO: 280 character maximum
-    reactionBody: { type: String, required: true},
+    reactionBody: { type: String, required: true, max: 280 },
     // TODO: createdAt
     username: { type: String, required: true},
-    // TODO: Date
-    // Set default value to the current timestamp
-    // Use a getter method to format the timestamp on query
+    // TODO: Date - Set default value to the current timestamp, Use a getter method to format the timestamp on query
     date: { type: Date, default: Date.now },
-  });
+  },
+  // TODO: virtual: true
+  );
 
 const thoughtSchema = new mongoose.Schema({
-    // TODO: // String
-    // Required
-    // Must be between 1 and 280 characters
-    // createdAt
-  thoughtText: { type: String, required: true },
-    // TODO: string, unique, required, valid email
+    // TODO: createdAt
+  thoughtText: { type: String, required: true, min: 1, max: 280 },
   date: {},
-    // String
-    // Required
-    // reactions (These are like replies)
+    // String required, reactions are like replies
   username: { type: String, required: true}, // TODO:Array of _id values referencing the Thought model
   reactions: [reactionSchema], // TODO: // Array of nested documents created with the reactionSchema
   lastAccessed: { type: Date, default: Date.now },
@@ -40,11 +32,9 @@ thoughtSchema.virtual('reactionCount').
 const Thought = mongoose.model('Thought', thoughtSchema);
 
 
-
-
 // Schema Settings:
 
 // This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
 
-
+ß
 module.exports = Thought;
