33782ead5bc54c58c834c17fee05b976b2cd7c91
emma@beinproximity.com <emma@beinproximity.com>
2022-05-07 11:31:21 -0600

    blah

--- develop/routes/api/userRoutes.js
+++ develop/routes/api/userRoutes.js
@@ -8,7 +8,7 @@ const {
   updateUser,
   deleteUser,
   addFriend,
-  deleteFriend
+  // deleteFriend
 } = require('../../controller/userController');
 
 router
@@ -26,6 +26,6 @@ router
 router
   .route('/:id/friends/:friendId')
     .post(addFriend)
-    .delete(deleteFriend);
+    // .delete(deleteFriend);
 
 module.exports = router; 
